d4bfb3137a91ea07ef22b952d8b5b73c0182bb47
Rob Bygrave <robin.bygrave@gmail.com>
2022-05-24 15:03:14 +1200

    Update dependencies

---
+++ kate/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module io.avaje.kate {
+
+    requires transitive io.avaje.http.api;
+    requires transitive io.avaje.config;
+    requires transitive io.avaje.inject;
+    requires transitive io.avaje.jsonb;
+    requires transitive io.avaje.jex;
+    requires transitive io.avaje.jex.jetty;
+    //requires io.avaje.http.client;
+
+}
